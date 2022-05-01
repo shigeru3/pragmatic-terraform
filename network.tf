3803bceefb6ab91f72aca46659602f2e02ebe80a
shigeru <hironoki3@gmail.com>
2022-05-01 19:42:35 +0900

    pragmatic terraform #7 networking

--- network.tf
+++ network.tf
@@ -8,11 +8,18 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "public" {
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "public_0" {
+  cidr_block = "10.0.1.0/24"
   vpc_id     = aws_vpc.example.id
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_1" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.example.id
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_internet_gateway" "example" {
@@ -29,7 +36,78 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_0" {
   route_table_id = aws_route_table.public.id
-  subnet_id = aws_subnet.public.id
+  subnet_id = aws_subnet.public_0.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public_1.id
+}
+
+resource "aws_subnet" "private_0" {
+  cidr_block = "10.0.65.0/24"
+  vpc_id     = aws_vpc.example.id
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_1" {
+  cidr_block = "10.0.66.0/24"
+  vpc_id     = aws_vpc.example.id
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  subnet_id = aws_subnet.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  subnet_id = aws_subnet.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  subnet_id = aws_subnet.public_0.id
+  allocation_id = aws_eip.nat_gateway_0.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  subnet_id = aws_subnet.public_1.id
+  allocation_id = aws_eip.nat_gateway_1.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
 }
