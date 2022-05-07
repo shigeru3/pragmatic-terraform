f6fb50b85c5a004fc13d3d65f4630a2c3a8a45a6
shigeru <hironoki3@gmail.com>
2022-05-07 17:48:09 +0900

    pragmatic terraform #10 ECS Scheduled Task

--- ecs.tf
+++ ecs.tf
@@ -74,3 +74,54 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy = data.aws_iam_policy_document.ecs_task_execution.json
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_ecs_task_definition" "example_batch" {
+  container_definitions = file("./batch_container_definitions.json")
+  family                = "example-batch"
+  cpu = "256"
+  memory = "512"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn = module.ecs_task_execution_role.iam_role_arn
+}
+
+module "ecs_events_role" {
+  source = "./iam_role"
+  name = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
+
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name = "example-batch"
+  description = "Very important batch"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "example_batch" {
+  arn  = aws_ecs_cluster.example.arn
+  rule = aws_cloudwatch_event_rule.example_batch.name
+  target_id = "example-batch"
+  role_arn = module.ecs_events_role.iam_role_arn
+
+  ecs_target {
+    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+    launch_type = "FARGATE"
+    task_count = 1
+    platform_version = "1.3.0"
+
+    network_configuration {
+      subnets = [aws_subnet.private_0.id]
+      assign_public_ip = "false"
+    }
+  }
+}
